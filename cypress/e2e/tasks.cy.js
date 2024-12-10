0a900930a5fb0ff2806fa6eae9b06b8f5977bfe1
Fabio Régis <fabioregis@Fabios-MacBook-Air.local>
2024-12-10 14:30:00 -0300

    new exercises

--- cypress/e2e/tasks.cy.js
+++ cypress/e2e/tasks.cy.js
@@ -2,9 +2,33 @@
 
 describe('tarefas', ()=> {
     it('deve cadastrar uma nova tarefa', ()=>{
-        cy.visit('http://localhost:3000')
-        cy.get('input[placeholder="Add a new Task"]').type('Ler um livro de node.js')
 
-        cy.contains('button',"Create").click()
+        const taskName = 'Ler um livro de node.js'
+
+        cy.removeTaskByName(taskName)
+        cy.createTask(taskName)
+
+        cy.contains('main div p' , taskName).should('be.visible')
+    })
+
+    it('não deve permitir tarefa duplicada', ()=>{
+
+        const task = {
+            name : 'Estudar JavaScript',
+            is_done : false
+        }
+
+        cy.removeTaskByName(task.name)
+        cy.postTask(task)
+        cy.createTask(task.name)
+    
+        cy.get('.swal2-html-container').should('be.visible').should('have.text','Task already exists!')
+    })
+
+    it('campo obrigatório', ()=>{
+
+        cy.createTask()
+        cy.isRequired('This is a required field')
+
     })
 })
